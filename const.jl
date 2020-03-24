be08254a6956fc6856d59a7d44a5a93a9a26e46b
Goetterwind <d.albach@hzdr.de>
2020-03-24 15:51:03 +0100

    structs

--- const.jl
+++ const.jl
@@ -10,3 +10,18 @@ c_0 = 299792458 # speed of light in vacuum [m/s]
 h = 6.626e-34  # Planck constant [Js]
 
 #include here some material constants, mainly for comparison
+
+#Yb:YAG
+N1per = 1.388e20
+
+#structs to contain data
+
+struct crystal
+    #content
+    length
+end
+
+struct laserparameters
+    #content of the different lasers for pump and extraction
+    wavelength
+end
