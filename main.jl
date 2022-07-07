8f0762256ceec4ba2f4e0d2fda22b88953ce2e57
Daniel Albach <d.albach@hzdr.de>
2022-07-07 16:57:46 +0200

    20220707 progresslogging interpolate spectra

--- main.jl
+++ main.jl
@@ -7,9 +7,11 @@
 
 #using section
 using ProgressMeter
+using ProgressLogging
 using DelimitedFiles
 using Plots
 using Interpolations
+
 
 # fundamental constants
 include("const.jl")
@@ -28,6 +30,7 @@ spectra_abs = readdlm(filepath)
 
 # clear the NaNs that are potentially hidden inside the spectrum
 spectra_abs[:,2] = replace!(spectra_abs[:,2], NaN => 0)
+spectra_abs_ip = interpolate((spectra_abs[:,1],), spectra_abs[:,2], Gridded(Linear()))
 
 #read the fluorescence data
 filename = "JK_CaF300Kf.txt"
@@ -39,8 +42,11 @@ spectra_flu = readdlm(filepath)
 # clear the NaNs that are potentially hidden inside the spectrum
 spectra_flu[:,2] = replace!(spectra_flu[:,2], NaN => 0)
 
+# do some linear interpolation in the interesting range
+spectra_flu_ip = interpolate((spectra_flu[:,1],), spectra_flu[:,2], Gridded(Linear()))
+
 # now make a nice plot of the spectra overlaying each other
-plot(spectra_abs[:,1],spectra_abs[:,2], ylims=(0,1e-20))
+plot(spectra_abs[:,1],spectra_abs[:,2], ylims=(0,1e-20))s
 plot!(spectra_flu[:,1],spectra_flu[:,2], ylims=(0,1e-20))
 
 #println("constant speed of light $c_light")
@@ -48,18 +54,18 @@ plot!(spectra_flu[:,1],spectra_flu[:,2], ylims=(0,1e-20))
 #scope is a bit of a pain - use let end or define as globals
 #let
     #ProgreesMeter call
-    p=Progress(steps_crystal, dt=0.05, color=:grey)
+    # p=Progress(steps_crystal, dt=0.05, color=:grey)
 
     a=0
     b=0
 
-    for i in 1:steps_crystal
+    @progress for i in 1:steps_crystal
         #sleep(0.1)
         global a
         global b
         a,b = test_function(i,steps_crystal)
-        ProgressMeter.next!(p)
-        # sleep(0.1)
+        # ProgressMeter.next!(p)
+        sleep(0.02)
     end
     println(a);
 #end
